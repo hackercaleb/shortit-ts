916ddcdb3db1fb25025eb70e67c5cb8447ef2a87
John Oshalusi <john.oshalusi@iohk.io>
2024-04-03 00:00:44 +0100

    setup linting and improve app config

---
+++ README.md
@@ -0,0 +1,7 @@
+# Shortit - URL Shortener
+
+Shortit is a URL shortener service that allows you to shorten long URLs to a shorter, more user-friendly URL. It is built using Typescript and Node.
+
+## Commands
+- `yarn dev` - Start the development server
+- `nvm use` - Use the correct Node version
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import express, { Express, Request, Response, Application } from "express";
-import dotenv from "dotenv";
+import express, { Request, Response, Application } from 'express';
+import dotenv from 'dotenv';
 
 //For env File
 dotenv.config();
@@ -7,10 +7,10 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to Express & TypeScript Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Shortit URL Shortner API');
 });
 
 app.listen(port, () => {
-  console.log(`Server is Fire at http://localhost:${port}`);
+  console.log(`Server live at http://localhost:${port}`);
 });
